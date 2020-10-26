4c3f1e68ae9cf57a0cc79514b7532726d938bfe3
MegaMoth <Simot18@student.sdu.dk>
2020-10-26 04:08:54 +0100

    HTTP request response working

--- client/src/main/java/dk/vip/client/persistence/TailTransmissionHandler.java
+++ client/src/main/java/dk/vip/client/persistence/TailTransmissionHandler.java
@@ -1,12 +1,32 @@
 package dk.vip.client.persistence;
 
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import dk.vip.client.domain.HeadTransmissionHandler;
 
 public class TailTransmissionHandler implements HeadTransmissionHandler {
 
     @Override
-    public String transmit(String expression) {
-        
-        return null;
+    public String transmit(String expressionJson) {
+        String[] result = new String[1];
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+              .uri(URI.create("https://postman-echo.com/get"))
+              .build();
+        client.sendAsync(request, BodyHandlers.ofString())
+              .thenApply(HttpResponse::body)
+              .thenAccept(s -> result[0] = s)
+              .join();
+
+        return result[0];
     }
 }
