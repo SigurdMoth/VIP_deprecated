ac9f01bf1230ebb512492cc8c07b8a5942518c0d
MegaMoth <Simot18@student.sdu.dk>
2020-11-09 23:45:32 +0100

    Small fixes, port must be changed for session and
    protocolbroker to run on same pc

--- session/src/main/java/dk/vip/session/persistence/TransmissionHandlerImp.java
+++ session/src/main/java/dk/vip/session/persistence/TransmissionHandlerImp.java
@@ -1,9 +1,12 @@
 package dk.vip.session.persistence;
 
+import org.springframework.stereotype.Service;
+
 import dk.vip.session.domain.transmit.ITransmissionHandler;
 import dk.vip.transmit.Transmission;
 import dk.vip.wrap.Wrap;
 
+@Service
 public class TransmissionHandlerImp implements ITransmissionHandler {
 
     @Override
